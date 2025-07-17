ab0e8ba9c52e660ff780e751efd61e6df1f22104
demetalgimp <sean.erin.walton@gmail.com>
2025-07-17 15:42:59 -0600

    + Got calendar fragment to finally work.

---
+++ app/src/main/java/com/waltoncraftsllc/waterfrontcashflow/VerticalTextView.java
@@ -0,0 +1,46 @@
+package com.waltoncraftsllc.waterfrontcashflow;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.Gravity;
+
+public class VerticalTextView extends androidx.appcompat.widget.AppCompatTextView {
+    final boolean topDown;
+    public VerticalTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        final int gravity = getGravity();
+        topDown = ( Gravity.isVertical(gravity)  &&  (gravity & Gravity.VERTICAL_GRAVITY_MASK) == Gravity.BOTTOM );
+        if ( !topDown ) {
+            setGravity( (gravity & Gravity.HORIZONTAL_GRAVITY_MASK) | Gravity.TOP);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    @Override
+    protected void onDraw( Canvas canvas ) {
+        TextPaint textPaint = getPaint();
+        textPaint.setColor( getCurrentTextColor() );
+        textPaint.drawableState = getDrawableState();
+
+        canvas.save();
+
+        if ( topDown ) {
+            canvas.translate( getWidth(), 0 );
+            canvas.rotate( 90 );
+        } else {
+            canvas.translate( 0, getHeight() );
+            canvas.rotate( -90 );
+        }
+
+        canvas.translate(getCompoundPaddingLeft(), getExtendedPaddingTop());
+        getLayout().draw(canvas);
+        canvas.restore();
+    }
+}
--- app/src/main/java/com/waltoncraftsllc/waterfrontcashflow/fragments/CalendarFragment.java
+++ app/src/main/java/com/waltoncraftsllc/waterfrontcashflow/fragments/CalendarFragment.java
@@ -1,5 +1,8 @@
 package com.waltoncraftsllc.waterfrontcashflow.fragments;
 
+import static java.time.YearMonth.now;
+
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.waltoncraftsllc.waterfrontcashflow.R;
+import com.waltoncraftsllc.waterfrontcashflow.VerticalTextView;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.TextStyle;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,19 +38,22 @@ public class CalendarFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    TextView mView_CurrentMonth;
+    VerticalTextView mView_PreviousMonth;
+    VerticalTextView mView_NextMonth;
 
     public CalendarFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
-     */
+/**
+ * Use this factory method to create a new instance of
+ * this fragment using the provided parameters.
+ *
+ * @param param1 Parameter 1.
+ * @param param2 Parameter 2.
+ * @return A new instance of fragment CalendarFragment.
+ */
     // TODO: Rename and change types and number of parameters
     public static CalendarFragment newInstance(String param1, String param2) {
         CalendarFragment fragment = new CalendarFragment();
@@ -50,8 +63,8 @@ public class CalendarFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
-    TextView[] mCalendarTextViews;
+    private static TextView[] mCalendarTextViews;
+    private LocalDate mThisMonth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,6 +72,53 @@ public class CalendarFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    private void writeMonth(TextView view, LocalDate date) {
+        Month month = date.getMonth();
+        String month_name = month.getDisplayName(TextStyle.FULL, Locale.US);
+        view.setText(String.format(Locale.US, "  %s (%04d)  ", month_name, date.getYear()));
+    }
+
+    /** private void fillCalendarView(LocalDate date)
+     * Fill
+     * @param date
+     */
+    private void fillCalendarView(LocalDate date) {
+        mThisMonth = date;
+        int start_day_of_month = LocalDate.of(date.getYear(), date.getMonth(), 1).getDayOfWeek().getValue();
+        int days_in_current_month = mThisMonth.lengthOfMonth();
+        int days_in_previous_month = mThisMonth.minusMonths(1).lengthOfMonth();
+/******************************************************************************************************
+                                                  [0...(start_day_of_month - 1)] = previous month
+         [start_day_of_month...(start_day_of_month + days_in_current_month - 1)] = current month
+[(start_day_of_month + days_in_current_month)...(mCalendarTextViews.length - 1)] = next month
+*******************************************************************************************************/
+        for ( int i = 0; i < mCalendarTextViews.length; i++ ) {
+            int background = 0xF03030;
+            if ( i < start_day_of_month ) {
+                mCalendarTextViews[i].setText(String.valueOf(i + days_in_previous_month - start_day_of_month + 1));
+            }
+            if ( start_day_of_month <= i  &&  i < start_day_of_month + days_in_current_month ) {
+                mCalendarTextViews[i].setText(String.valueOf(i - start_day_of_month + 1));
+                background = 0x0000FF;
+            }
+            if ( (start_day_of_month + days_in_current_month) <= i  &&  i < mCalendarTextViews.length ) {
+                mCalendarTextViews[i].setText(String.valueOf(i - (days_in_current_month + start_day_of_month) + 1));
+            }
+            mCalendarTextViews[i].setBackgroundColor(background);
+        }
+
+        writeMonth(mView_CurrentMonth, mThisMonth);
+        writeMonth(mView_PreviousMonth, mThisMonth.minusMonths(1));
+        writeMonth(mView_NextMonth, mThisMonth.plusMonths(1));
+
+        LocalDate now = LocalDate.now();
+        if ( mThisMonth.getMonth() == now.getMonth()  &&  mThisMonth.getYear() == now.getYear() ) {
+            int current_day = mThisMonth.getDayOfMonth();
+            int index_to_TextView = (7 - mThisMonth.getDayOfWeek().getValue()) + current_day - 1;
+            mCalendarTextViews[index_to_TextView - 1].setBackgroundColor(0);
         }
     }
 
@@ -72,28 +132,40 @@ public class CalendarFragment extends Fragment {
                 v.setBackgroundColor(0);
             });
         }
+        mView_CurrentMonth = view.findViewById(R.id.textView_current_month_name);
+        mView_PreviousMonth = view.findViewById(R.id.view_previous_month_name);
+        mView_PreviousMonth.setOnClickListener(v -> {
+            mThisMonth = mThisMonth.minusMonths(1);
+            fillCalendarView(mThisMonth);
+        });
+        mView_NextMonth = view.findViewById(R.id.view_next_month_name);
+        mView_NextMonth.setOnClickListener(v -> {
+            mThisMonth = mThisMonth.plusMonths(1);
+            fillCalendarView(mThisMonth);
+        });
+        LocalDate date = java.time.LocalDate.now();
+        fillCalendarView(date);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
     //            R.id.view_Calendar_name,
     final int[] mCalendarIDs = {
             R.id.textView_Sunday_week1, R.id.textView_Monday_week1, R.id.textView_Tuesday_week1, R.id.textView_Wednesday_week1,
-            R.id.textView_Thursday_week1, R.id.textView_Friday_week1, R.id.textView_Saturday_week1,
+                R.id.textView_Thursday_week1, R.id.textView_Friday_week1, R.id.textView_Saturday_week1,
             R.id.textView_Sunday_week2, R.id.textView_Monday_week2, R.id.textView_Tuesday_week2, R.id.textView_Wednesday_week2,
-            R.id.textView_Thursday_week2, R.id.textView_Friday_week2, R.id.textView_Saturday_week2,
+                R.id.textView_Thursday_week2, R.id.textView_Friday_week2, R.id.textView_Saturday_week2,
             R.id.textView_Sunday_week3, R.id.textView_Monday_week3, R.id.textView_Tuesday_week3, R.id.textView_Wednesday_week3,
-            R.id.textView_Thursday_week3, R.id.textView_Friday_week3, R.id.textView_Saturday_week3,
+                R.id.textView_Thursday_week3, R.id.textView_Friday_week3, R.id.textView_Saturday_week3,
             R.id.textView_Sunday_week4, R.id.textView_Monday_week4, R.id.textView_Tuesday_week4, R.id.textView_Wednesday_week4,
-            R.id.textView_Thursday_week4, R.id.textView_Friday_week4, R.id.textView_Saturday_week4,
+                R.id.textView_Thursday_week4, R.id.textView_Friday_week4, R.id.textView_Saturday_week4,
             R.id.textView_Sunday_week5, R.id.textView_Monday_week5, R.id.textView_Tuesday_week5, R.id.textView_Wednesday_week5,
-            R.id.textView_Thursday_week5, R.id.textView_Friday_week5, R.id.textView_Saturday_week5,
+                R.id.textView_Thursday_week5, R.id.textView_Friday_week5, R.id.textView_Saturday_week5,
             R.id.textView_Sunday_week6, R.id.textView_Monday_week6, R.id.textView_Tuesday_week6, R.id.textView_Wednesday_week6,
-            R.id.textView_Thursday_week6, R.id.textView_Friday_week6, R.id.textView_Saturday_week6
+                R.id.textView_Thursday_week6, R.id.textView_Friday_week6, R.id.textView_Saturday_week6
     };
 }
 
